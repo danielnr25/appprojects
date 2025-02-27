684f8b561b37e24ddea687c7853fc133088fc1a6
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-02-26 19:57:12 -0500

    feat:semana11

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,18 +2,22 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Login } from "../interfaces/login";
 import { Observable } from "rxjs";
-
+import { environment } from "@env/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class LoginService {
+
+  urlApi:string = environment.api
+
   http = inject(HttpClient) // me permite hacer peticiones http a un servidor
   constructor() { }
 
   autenticar(data:Login):Observable<Login>{
-    return this.http.post<Login>('http://127.0.0.1:8000/api/autenticar',data);
+    //return this.http.post<Login>('http://127.0.0.1:8000/api/autenticar',data);
+    return this.http.post<Login>(`${this.urlApi}/autenticar`,data);
   }
 
   estadoAutenticado(){
