4296bbadd2663c70550669f76bc51fc4122789c5
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-03-05 20:17:06 -0500

    feat:semana12

--- src/app/admin/area/area.component.ts
+++ src/app/admin/area/area.component.ts
@@ -1,11 +1,196 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,Input } from '@angular/core';
+import { DxDataGridComponent, DxDataGridModule, DxFormModule, DxPopupModule, DxProgressBarModule } from 'devextreme-angular';
+import DataSource from 'devextreme/data/data_source';
+import notify from 'devextreme/ui/notify';
+import {Area} from '@interfaces/area';
+import {AreaService} from '@services/area.service';
 
+const MODULES_DEVEXTREME = [DxDataGridModule,DxFormModule,DxPopupModule,DxProgressBarModule];
 @Component({
   selector: 'app-area',
-  imports: [],
+  imports: [MODULES_DEVEXTREME],
   templateUrl: './area.component.html',
   styleUrl: './area.component.css'
 })
+
 export class AreaComponent {
+  @ViewChild(DxDataGridComponent, { static: true }) dataGrid?: DxDataGridComponent;
+  @Input() idproyecto:number=0;
+  customersData: any;
+  shippersData: any;
+  dataSource;
+  url: string='';
+  masterDetailDataSource: any;
+
+  opcionesNuevo:any={
+    icon: 'add',
+    text: 'Agregar Area',
+    onClick: () => {
+      this.elementoArea = {
+        idarea:0,
+        nombre:'',
+        comentario:'',
+        estado:'',
+        idproyecto:0
+      }
+      console.log("Nueva area");
+      this.popupVisible = true;
+    },
+  };
+
+  opcionesEliminar:any={
+    icon: 'remove',
+    text: 'Eliminar Area',
+    onClick: () => {
+      console.log("Eliminar area");
+      this.eliminarSeleccionados();
+    },
+  };
+
+  editorOptions = { placeholder: 'Search column' };
+  popupVisible:boolean=false;
+  elementoArea:Area ={
+    idarea:0,
+    nombre:'',
+    comentario:'',
+    estado :'',
+    idproyecto:0
+  }
+
+  botonGuardarOpciones={
+    width: 300,
+    text: 'Guardar Area',
+    type: 'default',
+    stylingMode: 'contained',
+    onClick: () => {
+      this.popupVisible = false;
+      this.fnGuardar();
+    },
+  }
+
+  showProgressBar:boolean=false;
+  progressValue:number=0;
+  readonly allowedPageSizes:any = [5,10, 30, 100, 'all'];
+  showPageSizeSelector = true;
+  showInfo = true;
+  showNavButtons = true;
+
+  constructor(private servicioArea:AreaService){
+
+  }
+
+  fnEditar(e:any){
+    this.elementoArea = e.row.data;
+    this.popupVisible = true;
+  }
+
+  obtenerAlto(){
+    return window.innerHeight;
+  }
+
+  fnGuardar(){
+    if(this.elementoArea.idarea > 0 ){
+      this.servicioArea.actualizarArea(this.elementoArea).subscribe({
+        next: data => {
+          console.log(data);
+          notify({
+            message: 'Se actualizo de manera correcta',
+            type:'success',
+            displayTime: 1000,
+            animation: {
+              show: {
+                type: 'fade', duration: 400, from: 0, to: 1,
+              },
+              hide: { type: 'fade', duration: 40, to: 0 },
+            },
+          });
+          this.refresh();
+        },
+        error: err => {
+          console.log(err)
+        }
+      });
+    }else{
+      this.servicioArea.agregarArea(this.elementoArea).subscribe({
+        next: data => {
+          console.log(data);
+          notify({
+            message: 'Se guardo de manera correcta',
+            type:'success',
+            displayTime: 1000,
+            animation: {
+              show: {
+                type: 'fade', duration: 400, from: 0, to: 1,
+              },
+              hide: { type: 'fade', duration: 40, to: 0 },
+            },
+          });
+          this.refresh();
+        },
+        error: err => {
+          console.log(err)
+        }
+      });
+    }
+    this.refresh();
+  }
+
+  eliminarSeleccionados(){
+    const seleccionados :any = this.dataGrid?.instance.getSelectedRowsData();
+    if(seleccionados.length) === 0{
+      notify({
+        message: 'Debe seleccionar un registro a eliminar',
+        type:'info',
+        displayTime: 1000,
+        animation: {
+          show: {
+            type: 'fade', duration: 400, from: 0, to: 1,
+          },
+          hide: { type: 'fade', duration: 40, to: 0 },
+        },
+      });
+      return;
+
+      this.showProgressBar=true;
+
+      const totalItems = seleccionados.length;
+      let itemsEliminados = 0;
+      seleccionados.forEach((item:any, index:any) => {
+
+        let datosEliminar:any={
+          idarea:item.idarea
+        };
+
+        this.servicioArea.eliminarArea(datosEliminar).subscribe({
+          next: data => {
+            console.log('data',data);
+            itemsEliminados++;
+            this.progressValue = (itemsEliminados / totalItems) * 100;
+
+            // Si todos los elementos han sido eliminados, oculta la barra de progreso
+            if (itemsEliminados === totalItems) {
+              this.showProgressBar = false;
+              this.progressValue = 0;
+
+              notify("Se elimino correctamente", 'success', 2000);
+              this.refresh();
+            }
+          },
+          error: errores => {
+            console.log(errores);
+          }
+        });
+      });
+
+    }
+
+  }
+
+  refresh(){
+    this.dataGrid?.instance.refresh();
+  }
+
+
+
 
 }
--- src/app/admin/proyecto/proyecto.component.ts
+++ src/app/admin/proyecto/proyecto.component.ts
@@ -1,11 +1,179 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DxButtonModule, DxDataGridComponent, DxDataGridModule, DxFormModule, DxPopupModule, DxProgressBarModule, DxRadioGroupModule, DxScrollViewModule, DxTabsModule, DxToolbarModule } from 'devextreme-angular';
+import DataSource from 'devextreme/data/data_source';
+import notify from 'devextreme/ui/notify';
+
+import { Proyecto } from '@interfaces/proyecto';
+import { Tarea } from '@interfaces/tarea';
+
+import { ProyectoService } from '@services/proyecto.service';
+import { TipoProyectoService } from '@services/tipo-proyecto.service';
+
+import { AreaComponent } from '../area/area.component';
+import { MiembroComponent } from '../miembro/miembro.component';
+import { EstadoComponent } from '../estado/estado.component';
+import { EtapaComponent } from '../etapa/etapa.component';
+import { UserAvatarComponent } from '@shared/user-avatar/user-avatar.component';
+import { TareaKanbanComponent } from '@shared/tarea-kanban/tarea-kanban.component';
+import { AdminTareaListaComponent } from '@shared/admin-tarea-lista/admin-tarea-lista.component';
+
+const MODULES_EXTREME = [DxDataGridModule,DxFormModule,DxPopupModule,DxProgressBarModule,DxScrollViewModule,DxTabsModule,DxButtonModule,DxToolbarModule,DxRadioGroupModule];
 
 @Component({
   selector: 'app-proyecto',
-  imports: [],
+  imports: [MODULES_EXTREME],
   templateUrl: './proyecto.component.html',
   styleUrl: './proyecto.component.css'
 })
+
 export class ProyectoComponent {
+  @ViewChild(DxDataGridComponent, { static: true }) dataGrid?: DxDataGridComponent;
+  customersData: any;
+  shippersData: any;
+
+  url: string='';
+  masterDetailDataSource: any;
+  editorOptions = { placeholder: 'Search column' };
+  popupVisible:boolean=false;
+
+  showProgressBar:boolean=false;
+  progressValue:number=0;
+
+  readonly allowedPageSizes:any = [5,10, 30, 100, 'all'];
+  showPageSizeSelector = true;
+  showInfo = true;
+  showNavButtons = true;
+
+  listaTiposProyecto:any=[];
+  taskPanelItems:any[] = [
+    {text: 'Informacion'},
+    {text: 'Areas'},
+    {text: 'Etapas'},
+    {text: 'Estados'},
+    {text: 'Miembros'},
+    {text: 'Panel Canva de Tareas'}
+  ]
+
+  mostrarInformacionProyecto:boolean=true;
+  mostrarEstado:boolean=false;
+  mostrarKanba:boolean=false;
+  mostrarArea:boolean=false;
+  mostrarEtapa:boolean=false;
+  mostrarMiembro:boolean=false;
+
+  misTareas:Tarea[] =[{
+    idtarea:0,
+    nombre:'Tarea 01',
+    comentario:'',
+    idestado:1,
+    idproyecto:1,
+    idarea:1,
+    idetapa:1,
+    idmiembro:1,
+    area:'Area 01',
+    etapa:'Etapa 01',
+    miembro:'Miembro 01',
+    estado:'Pendiente'
+  }]
+
+  opcionesNuevo:any={
+    icon: 'add',
+    text: 'Agregar Proyecto',
+    onclick: () =>{
+      this.elementoProyecto = {
+        idproyecto:0,
+        nombre:'',
+        idtipoProyecto:0,
+        fechaInicio:new Date(),
+        fechaFin:new Date(),
+        detalle:'',
+        estado:'',
+        idusuario:1
+      }
+      console.log("Nuevo proyecto");
+      this.popupVisible = true;
+      if(this.elementoProyecto.idproyecto>0){
+        this.taskPanelItems = [
+          { text: 'Proyecto'},
+          { text: 'Areas'},
+          { text: 'Etapas'},
+          { text: 'Estados'},
+          { text: 'Miembros'},
+          {text: 'Panel Canva'}
+        ]
+      }else{
+        this.taskPanelItems = [
+          {
+            text: 'Proyecto',
+          }
+        ]
+      }
+    }
+  }
+
+  opcionesEliminar:any = {
+    icon: 'remove',
+    text: 'Eliminar Proyecto',
+    onClick: () => {
+      console.log("Eliminar proyecto");
+      this.eliminarSeleccionados();
+    },
+  }
+
+  elementoProyecto:Proyecto={
+    idproyecto:0,
+    nombre:'',
+    idtipoProyecto:0,
+    fechaInicio:new Date(),
+    fechaFin:new Date(),
+    detalle:'',
+    estado:'',
+    idusuario:1
+  }
+
+  botonGuardarOpciones = {
+    width: 300,
+    text: 'Guardar Proyecto',
+    type: 'default',
+    stylingMode: 'contained',
+    onClick: () => {
+      this.popupVisible = false;
+      this.fnGuardar();
+    },
+  }
+
+
+
+  constructor(){
+
+  }
+
+  fnEditar(){
+
+  }
+
+  obtenerAlto(){
+
+  }
+
+  fnGuardar(){
+
+  }
+
+  eliminarSeleccionados(){
+
+  }
+
+  refresh(){
+
+  }
+
+  fnCancelar(){
+
+  }
+
+  tabValueChange(){
+
+  }
 
 }
